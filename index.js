34c043fb2ed55446beeebf241a6670681b02acd8
Hojiakbar <hojiakbarrasulovich@gmail.com>
2023-04-24 15:25:37 +0500

    validation

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import { create, engine } from "express-handlebars";
 import AuthRoutes from "./routes/auth.js";
 import ProductsRoutes from "./routes/products.js";
+import flash from 'connect-flash'
+import session from 'express-session'
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -19,6 +21,8 @@ app.set("views", "./views");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
+app.use(session({secret: 'Hoji', resave: false, saveUninitialized:false}))
+app.use(flash())
 
 app.use(AuthRoutes);
 app.use(ProductsRoutes);
--- routes/auth.js
+++ routes/auth.js
@@ -7,25 +7,65 @@ const router = Router()
 router.get('/login', (req, res)=>{
     res.render('login', {
         title:'login | Hojiakbar',
-        isLogin:true
+        isLogin:true,
+        loginError:req.flash('loginError')
     })
 })
 
 router.get('/register', (req, res)=>{
     res.render('register', {
         title:'register | Hojiakbar',
-        isRegister:true
+        isRegister:true,
+        registerError:req.flash('registerError')
     })
 })
 
-router.post('/login', (req, res)=>{
-    console.log(req.body)
+router.post('/login', async (req, res)=>{
+    const {email, password} = req.body
+    if(!email || !password){
+        req.flash('loginError', "All fields is required")
+        res.redirect(('/login'))
+        return
+    }
+    const existUser = await User.findOne({email})
+    if(!existUser) {
+        req.flash('loginError', "User not found")
+        res.redirect(('/login'))
+        return
+    }
+
+    const isPassEqual = await bcrypt.compare(password, existUser.password)
+    if(!isPassEqual) {
+        req.flash('loginError', "Password wrong")
+        res.redirect(('/login'))
+        return
+    }
+
+    console.log(existUser)
+
     res.redirect('/')
 })
 
 router.post('/register', async (req, res)=>{
-    console.log(req.body)
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+  const {firstName, lastName, email, password } = req.body
+
+    if(!firstName || !lastName || !email || !password){
+        req.flash('registerError', "All fields is required")
+        res.redirect(('/register'))
+        return
+    }
+
+const cadidate = await User.findOne({email})
+
+    if(cadidate){
+        req.flash('registerError', "User already exist")
+        res.redirect(('/register'))
+        return
+    }
+
+
+
+    const hashedPassword = await bcrypt.hash(password, 10)
     const userData = {
         firstName:req.body.firstName,
         lastName:req.body.lastName,
