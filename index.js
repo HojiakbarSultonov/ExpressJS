ff8809c5d002d1fd02c94251f805a0d5f86aa061
Hojiakbar <hojiakbarrasulovich@gmail.com>
2023-04-24 12:42:19 +0500

    MongoDB and mongoose

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-import express from 'express'
-import {create, engine} from 'express-handlebars'
-import AuthRoutes from './routes/auth.js'
-import ProductsRoutes from './routes/products.js'
+import express from "express";
+import { create, engine } from "express-handlebars";
+import AuthRoutes from "./routes/auth.js";
+import ProductsRoutes from "./routes/products.js";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
+const app = express();
 
 const hbs = create({
-    defaultLayout:'main',
-    extname:'hbs'
-})
+  defaultLayout: "main",
+  extname: "hbs",
+});
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', './views')
-app.use(express.urlencoded({extended:true}))
-app.use(express.static('public'))
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
 
-app.use(AuthRoutes)
-app.use(ProductsRoutes)
+app.use(AuthRoutes);
+app.use(ProductsRoutes);
 
+const startApp = () => {
+  try {
+    mongoose.set("strictQuery", false);
 
+    mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
 
-
-
-
-
-const PORT = process.env.PORT || 4100
-app.listen(PORT, ()=>console.log(`Server is running : ${PORT}`))
+    const PORT = process.env.PORT || 4100;
+    app.listen(PORT, () => console.log(`Server is running : ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+startApp()
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 import {Router} from "express";
+import User from "../models/user.js";
 
 const router = Router()
 
@@ -22,8 +23,16 @@ router.post('/login', (req, res)=>{
     res.redirect('/')
 })
 
-router.post('/register', (req, res)=>{
+router.post('/register', async (req, res)=>{
     console.log(req.body)
+    const userData = {
+        firstName:req.body.firstName,
+        lastName:req.body.lastName,
+        email:req.body.email,
+        password:req.body.password,
+    }
+    const user =  await User.create(userData)
+    console.log(user);
     res.redirect('/')
 })
 export  default router
